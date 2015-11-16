9db5760fb4cf963f66f6c984785df890b45577ae
jasperjohns <alansaldanha@yahoo.com>
2015-11-16 10:45:06 -0600

    Added test and chnged Aync structure

--- app/src/androidTest/java/com/example/asaldanha/myapplication/ApplicationTest.java
+++ app/src/androidTest/java/com/example/asaldanha/myapplication/ApplicationTest.java
@@ -1,13 +1,79 @@
 package com.example.asaldanha.myapplication;
 
 import android.app.Application;
+import android.content.Context;
 import android.test.ApplicationTestCase;
+import android.test.UiThreadTest;
+import android.util.Pair;
+import android.widget.Toast;
+
+import java.net.URL;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
 public class ApplicationTest extends ApplicationTestCase<Application> {
+
+    EndpointsAsyncTask2 epAyncTask2;
+    CountDownLatch signal;
+    String mJsonString = null;
+    Exception mError = null;
+
+
     public ApplicationTest() {
         super(Application.class);
     }
+
+
+    protected void setUp() throws Exception {
+        super.setUp();
+
+        signal = new CountDownLatch(1);
+//        gepAyncTask = new GEPAsyncTask(this);
+
+    }
+
+    @UiThreadTest
+    public void testDownload() throws InterruptedException
+    {
+
+
+        epAyncTask2 = new EndpointsAsyncTask2();
+        epAyncTask2.setListener(new EndpointsAsyncTask2.EndpointsListener2() {
+            @Override
+            public void onComplete(String jsonString, Exception e) {
+                mJsonString = jsonString;
+                mError = e;
+                signal.countDown();
+
+            }
+        }).execute("Manfred2");
+       signal.await(30, TimeUnit.SECONDS);
+        assertEquals("Hi, Manfred2", mJsonString);
+
+
+
+/*
+        epAyncTask = new EndpointsAsyncTask();
+        epAyncTask.setListener(new EndpointsAsyncTask.EndpointsListener() {
+            @Override
+            public void onComplete(String jsonString, Exception e) {
+                mJsonString = jsonString;
+                mError = e;
+                signal.countDown();
+            }
+        }).execute(new Pair<Context, String>(this));
+        signal.await();
+
+//       signal.await(30, TimeUnit.SECONDS);
+
+//        assertTrue( "assert something meaningful here", true );
+        assertEquals(1, 1);
+*/
+    }
+
+
+
 }
--- app/src/main/java/com/example/asaldanha/myapplication/MainActivityFragment.java
+++ app/src/main/java/com/example/asaldanha/myapplication/MainActivityFragment.java
@@ -18,12 +18,22 @@ import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
 import com.google.myapplication.backend.myApi.MyApi;
 import android.widget.Toast;
 import java.io.IOException;
+import java.net.URL;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+
+    EndpointsAsyncTask epAyncTask;
+    EndpointsAsyncTask2 epAyncTask2;
+    CountDownLatch signal;
+    String mJsonString = null;
+    Exception mError = null;
+
 
     public MainActivityFragment() {
     }
@@ -32,49 +42,200 @@ public class MainActivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        new EndpointsAsyncTask().execute(new Pair<Context, String>(getActivity(), "Manfred"));
+
+        epAyncTask2 = new EndpointsAsyncTask2();
+        epAyncTask2.setListener(new EndpointsAsyncTask2.EndpointsListener2() {
+            @Override
+            public void onComplete(String jsonString, Exception e) {
+                mJsonString = jsonString;
+                mError = e;
+//                signal.countDown();
+                Toast.makeText(getActivity(), mJsonString, Toast.LENGTH_LONG).show();
+
+            }
+        }).execute("Manfred2");
+
+
+
+/*
+        epAyncTask = new EndpointsAsyncTask();
+        epAyncTask.setListener(new EndpointsAsyncTask.EndpointsListener() {
+            @Override
+            public void onComplete(String jsonString, Exception e) {
+                mJsonString = jsonString;
+                mError = e;
+//                signal.countDown();
+            }
+        }).execute(new Pair<Context, String>(getActivity(), "Manfred"));
+*/
+
+
+
+
+/*
+        try {
+            signal.await();
+
+        } catch (InterruptedException name) {
+
+        }
+
+*/
+
+
+
+
+
+//        new EndpointsAsyncTask().execute(new Pair<Context, String>(getActivity(), "Manfred"));
         return inflater.inflate(R.layout.fragment_main, container, false);
     }
 
 
-    class EndpointsAsyncTask extends AsyncTask<Pair<Context, String>, Void, String> {
-        private  MyApi myApiService = null;
-        private Context context;
 
-        @Override
-        protected String doInBackground(Pair<Context, String>... params) {
-            if(myApiService == null) {  // Only do this once
-                MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
-                        new AndroidJsonFactory(), null)
-                        // options for running against local devappserver
-                        // - 10.0.2.2 is localhost's IP address in Android emulator
-                        // - turn off compression when running against local devappserver
-                        .setRootUrl("http://10.0.2.2:8080/_ah/api/")
-                        .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
-                            @Override
-                            public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
-                                abstractGoogleClientRequest.setDisableGZipContent(true);
-                            }
-                        });
-                // end options for devappserver
 
-                myApiService = builder.build();
-            }
+}
 
-            context = params[0].first;
-            String name = params[0].second;
+class EndpointsAsyncTask2 extends AsyncTask<String, Void, String> {
+    private  MyApi myApiService = null;
+    private Context context;
 
-            try {
-                return myApiService.sayHi(name).execute().getData();
-            } catch (IOException e) {
-                return e.getMessage();
-            }
+
+    private EndpointsListener2 epListener;
+    private String name;
+    private Exception mError = null;
+
+
+    @Override
+    protected String doInBackground(String ... params) {
+        if(myApiService == null) {  // Only do this once
+            MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
+                    new AndroidJsonFactory(), null)
+                    // options for running against local devappserver
+                    // - 10.0.2.2 is localhost's IP address in Android emulator
+                    // - turn off compression when running against local devappserver
+                    .setRootUrl("http://10.0.2.2:8080/_ah/api/")
+                    .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+                        @Override
+                        public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
+                            abstractGoogleClientRequest.setDisableGZipContent(true);
+                        }
+                    });
+            // end options for devappserver
+
+            myApiService = builder.build();
         }
 
-        @Override
-        protected void onPostExecute(String result) {
-            Toast.makeText(context, result, Toast.LENGTH_LONG).show();
+//        context = params[0].first;
+        String name = params[0];
+
+
+//        String name = "something";
+
+
+        try {
+            return myApiService.sayHi(name).execute().getData();
+        } catch (IOException e) {
+            return e.getMessage();
         }
+    }
+
+    public EndpointsAsyncTask2 setListener(EndpointsListener2 listener) {
+        this.epListener = listener;
+        return this;
+    }
+
+
+
+    @Override
+    protected void onPostExecute(String result) {
+
+        if (this.epListener != null)
+            this.epListener.onComplete(result, mError);
+
+  //      Toast.makeText(context, result, Toast.LENGTH_LONG).show();
+    }
+
+    public interface EndpointsListener2
+    {
+//        void downloadCompleted();
+
+//        void processCompleted(String result);
+
+        public void onComplete(String jsonString, Exception e);
+
+
+    }
+
+
+}
+
+
+
+class EndpointsAsyncTask extends AsyncTask<Pair<Context, String>, Void, String> {
+    private  MyApi myApiService = null;
+    private Context context;
+
+
+    private EndpointsListener epListener;
+    private String name;
+    private Exception mError = null;
+
+
+    @Override
+    protected String doInBackground(Pair<Context, String>... params) {
+        if(myApiService == null) {  // Only do this once
+            MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
+                    new AndroidJsonFactory(), null)
+                    // options for running against local devappserver
+                    // - 10.0.2.2 is localhost's IP address in Android emulator
+                    // - turn off compression when running against local devappserver
+                    .setRootUrl("http://10.0.2.2:8080/_ah/api/")
+                    .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+                        @Override
+                        public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
+                            abstractGoogleClientRequest.setDisableGZipContent(true);
+                        }
+                    });
+            // end options for devappserver
+
+            myApiService = builder.build();
+        }
+
+        context = params[0].first;
+        String name = params[0].second;
+
+        try {
+            return myApiService.sayHi(name).execute().getData();
+        } catch (IOException e) {
+            return e.getMessage();
+        }
+    }
+
+    public EndpointsAsyncTask setListener(EndpointsListener listener) {
+        this.epListener = listener;
+        return this;
+    }
+
+
+
+    @Override
+    protected void onPostExecute(String result) {
+
+        if (this.epListener != null)
+            this.epListener.onComplete(result, mError);
+
+        Toast.makeText(context, result, Toast.LENGTH_LONG).show();
+    }
+
+    public interface EndpointsListener
+    {
+//        void downloadCompleted();
+
+//        void processCompleted(String result);
+
+        public void onComplete(String jsonString, Exception e);
+
+
     }
 
 
